d552cacd4744813c584063a20f352d233331af6f
bedo <bedo@gamil.com>
2023-08-11 14:34:39 -0700

    finished c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: write function
+ *
+ * Return: 1 (error).
+ */
+
+
+int main(void)
+{	char arr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, arr, 59)
+	return (1);
+}
