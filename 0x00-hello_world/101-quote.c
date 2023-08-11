520594baf602129a7f1a22ff999be494ca865cf8
bedo <bedo@gamil.com>
2023-08-11 15:06:52 -0700

    ll

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Description: write function
  *
  * Return: 1 (not Success)
@@ -12,6 +12,6 @@
 int main(void)
 {
 	char arr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write (1)
+	write(1, arr, 59)
 	return (1);
 }
