348ba499749b016155918f79b82152062b9ed4bf
bedo <bedo@gamil.com>
2023-08-15 13:48:15 -0700

    dsd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: dont understand
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(l[ch]);
+	_putchar('\n');
+
+	return (0);
+}
