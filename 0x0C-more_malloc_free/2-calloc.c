aa27d04ae8edb8c213958cd41cd556057e1195d9
bedo <bedo@gamil.com>
2023-09-08 08:44:42 -0700

    ds

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -31,12 +31,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *m;
 
-	if (size == 0 || nmeb == 0)
+	if (size == 0 || nmemb == 0)
 		return (NULL);
 	m = malloc(sizeof(int) * nmemb)
 
-		if (m == 0)
-			return (NULL);
+	if (m == 0)
+		return (NULL);
 	_memset(m, 0, sizeof(int) * nmemb);
 	return (m);
 
