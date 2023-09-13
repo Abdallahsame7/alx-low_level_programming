7c20004e16c1ef1df45e041470cb256f9be09acd
bedo <bedo@gamil.com>
2023-09-13 07:30:23 -0700

    ds

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - parm
+ * @array: int
+ * @size: size
+ * @action: void
+ * Return: void
+ */
+
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int *x = array + size - 1;
+
+	if (array && size && action)
+		while (array < = x)
+			action(*array++);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include "3-calc.h"
+
+/**
+ * op_add - parm
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - parm
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - parm
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - parm
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - parm
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
