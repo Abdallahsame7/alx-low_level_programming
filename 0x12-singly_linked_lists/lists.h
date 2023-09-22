c2531a5802bf7068aeefdc348301b923c41e8733
bedo <bedo@gamil.com>
2023-09-22 10:22:01 -0700

    ds

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef LISTS_H
-#define LISTS_H	
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,7 +21,7 @@ typedef struct list_s
 } list_t;
 
 int _putchar(char c);
-int _strlen(cahr *s);
+int _strlen(char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
