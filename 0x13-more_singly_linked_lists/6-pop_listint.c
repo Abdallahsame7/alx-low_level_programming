ad0c15dfbb9096e3b2190ff3417a28aa638feab8
bedo <bedo@gamil.com>
2023-09-25 06:27:31 -0700

    ds

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+
+/**
+ * pop_listint - head
+ * @head: pointer
+ * Return: int
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int n;
+
+	if (!head || !*head)
+		return (0);
+
+	node = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = node;
+	return (n);
+}
